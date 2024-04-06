cbba88abb68653959d91be99e28083ffdb2a7231
ujjawalPeak01 <119571874+ujjawalPeak01@users.noreply.github.com>
2024-04-06 16:24:03 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import json
+from transformers import pipeline
+
+
+class InferlessPythonModel:
+
+    def initialize(self):
+        self.generator = pipeline("text-generation", model="EleutherAI/gpt-neo-125M",device=0)
+        
+        print("This is Initialize Code", flush=True)
+
+    
+    def infer(self, inputs):
+        prompt = inputs["prompt"]
+        pipeline_output = self.generator(prompt, do_sample=True, min_length=20, max_length=300)
+        generated_txt = pipeline_output[0]["generated_text"]
+        return {"generated_text": generated_txt}
+
+    def finalize(self,args):
+        self.pipe = None
